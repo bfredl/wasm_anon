1a335b053884d9725994ad3bb4ad9a064342ca95
bfredl <bjorn.linse@gmail.com>
2024-11-16 18:41:17 +0100

    typiskt

--- src/wasm_shelf.zig
+++ src/wasm_shelf.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const testing = std.testing;
 const dbg = std.debug.print;
+const Reader = std.io.FixedBufferStream([]const u8).Reader;
 
-fn readLeb(r: anytype, comptime T: type) !T {
+fn readLeb(r: Reader, comptime T: type) !T {
     return switch (@typeInfo(T).int.signedness) {
         .signed => std.leb.readILEB128(T, r),
         .unsigned => std.leb.readULEB128(T, r),
@@ -26,6 +27,15 @@ const SectionKind = enum(u8) {
     _,
 };
 
+const ValType = enum(u8) {
+    int32 = 0x7F,
+    int64 = 0x7E,
+    float32 = 0x7D,
+    float64 = 0x7C,
+    vec128 = 0x7B,
+    _,
+};
+
 pub fn parse(module: []const u8) !void {
     if (module.len < 8) return error.InvalidFormat;
     if (!std.mem.eql(u8, module[0..8], &.{ 0, 'a', 's', 'm', 1, 0, 0, 0 })) return error.InvalidFormat;
@@ -40,9 +50,41 @@ pub fn parse(module: []const u8) !void {
         };
         const kind: SectionKind = @enumFromInt(id);
 
-        const len = try readLeb(&r, u32);
+        const len = try readLeb(r, u32);
         dbg("SECTION: {s} ({}) with len {}\n", .{ @tagName(kind), id, len });
-        try r.skipBytes(len, .{});
+        const end_pos = fbs.pos + len;
+        switch (kind) {
+            .type => try type_section(r),
+            else => {}, // try r.skipBytes(len, .{})
+        }
+
+        // TODO: this should be strict, but we are just fucking around and finding out for now
+        if (fbs.pos > end_pos) {
+            return error.InvalidFormat;
+        }
+        fbs.pos = end_pos;
+    }
+}
+
+pub fn type_section(r: Reader) !void {
+    const len = try readLeb(r, u32);
+    dbg("TYPES: {}\n", .{len});
+    for (0..len) |_| {
+        const tag = try r.readByte();
+        if (tag != 0x60) return error.InvalidFormat;
+        const n_params = try readLeb(r, u32);
+        dbg("(", .{});
+        for (0..n_params) |_| {
+            const typ: ValType = @enumFromInt(try r.readByte());
+            dbg("{s}, ", .{@tagName(typ)});
+        }
+        dbg("): (", .{});
+        const n_ret = try readLeb(r, u32);
+        for (0..n_ret) |_| {
+            const typ: ValType = @enumFromInt(try r.readByte());
+            dbg("{s}, ", .{@tagName(typ)});
+        }
+        dbg(")\n", .{});
     }
 }
 
