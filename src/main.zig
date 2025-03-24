0b7a2c2e40fe35bfe89083f0d59b0d100ebf4c9b
bfredl <bjorn.linse@gmail.com>
2025-03-24 21:09:46 +0100

    PLEASE CLAP

--- src/main.zig
+++ src/main.zig
@@ -11,18 +11,19 @@ const params = clap.parseParamsComptime(
     \\-h, --help             Display this help and exit.
     \\-f, --func <str>       call function
     \\-i, --inspect          inspect imports and exports
+    \\-s, --stats            Dump some stats on exit
     \\--stdin <str>          override wasi stdin
     \\<str>
     \\
 );
 
-pub fn main() !void {
+pub fn main() !u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
     const argv = std.os.argv;
     var diag = clap.Diagnostic{};
-    var param = clap.parse(clap.Help, &params, clap.parsers.default, .{
+    var p = clap.parse(clap.Help, &params, clap.parsers.default, .{
         .diagnostic = &diag,
         .allocator = gpa.allocator(),
     }) catch |err| {
@@ -30,53 +31,101 @@ pub fn main() !void {
         diag.report(std.io.getStdErr().writer(), err) catch {};
         return err;
     };
-    defer param.deinit();
+    defer p.deinit();
 
-    const filearg = param.positionals[0] orelse @panic("usage");
+    const filearg = p.positionals[0] orelse @panic("usage");
     const buf = try util.readall(allocator, filearg);
     defer allocator.free(buf);
 
     var mod = try wasm_shelf.Module.parse(buf, allocator);
     defer mod.deinit();
-    defer mod.dump_counts();
+    defer if (p.args.stats > 0) mod.dump_counts();
 
-    if (param.args.inspect > 0) {
+    if (p.args.inspect > 0) {
         try mod.dbg_imports();
         try mod.dbg_exports();
-        return;
+        return 0;
     }
 
-    if (param.args.func) |func| {
+    if (p.args.func) |func| {
         _ = func;
-        @panic("NYI");
+        const callname = std.mem.span(argv[2]);
+
+        var in = try Instance.init(&mod, null);
+        defer in.deinit();
+
+        const sym = try mod.lookup_export(callname) orelse {
+            dbg("not found :pensive:\n", .{});
+            return 1;
+        };
+
+        dbg("SYM: {}\n", .{sym});
+        if (sym.kind != .func) {
+            dbg("not a function :(\n", .{});
+            return 1;
+        }
+
+        const num = try std.fmt.parseInt(i32, std.mem.span(argv[3]), 10);
+        var res: [1]StackValue = undefined;
+        const n_res = try in.execute(sym.idx, &.{.{ .i32 = num }}, &res, true);
+        if (n_res != 1) dbg("TODO: n_res\n", .{});
+        dbg("{s}({}) == {}\n", .{ std.mem.span(argv[2]), num, res[0].i32 });
     } else {
-        return wasi_run(&mod, allocator, @ptrCast(param.args.stdin));
+        const status = try wasi_run(&mod, allocator, @ptrCast(p.args.stdin));
+        return @intCast(@min(status, 255));
+    }
+    return 0;
+}
+
+const WASIState = struct {
+    klocka: std.time.Timer,
+    exit_status: ?u32 = null,
+};
+
+fn wasi_run(mod: *wasm_shelf.Module, allocator: std.mem.Allocator, stdin: ?[:0]const u8) !u32 {
+    if (stdin) |path| {
+        const fd = try std.posix.openZ(path, .{ .ACCMODE = .RDONLY }, 0);
+        try std.posix.dup2(fd, 0);
     }
 
-    const callname = std.mem.span(argv[2]);
+    var imports: wasm_shelf.ImportTable = .init(allocator);
+    defer imports.deinit();
 
-    var in = try Instance.init(&mod, null);
+    var state: WASIState = .{ .klocka = try std.time.Timer.start() };
+
+    try imports.add_func("proc_exit", .{ .cb = &wasi_proc_exit, .n_args = 1, .n_res = 0, .data = @ptrCast(&state) });
+    try imports.add_func("fd_read", .{ .cb = &wasi_fd_read, .n_args = 4, .n_res = 1 });
+    try imports.add_func("fd_write", .{ .cb = &wasi_fd_write, .n_args = 4, .n_res = 1 });
+    try imports.add_func("clock_time_get", .{ .cb = &wasi_clock_time_get, .n_args = 3, .n_res = 1, .data = @ptrCast(&state) });
+
+    var in = try wasm_shelf.Instance.init(mod, &imports);
     defer in.deinit();
 
-    const sym = try mod.lookup_export(callname) orelse
-        return dbg("not found :pensive:\n", .{});
+    const sym = try mod.lookup_export("_start") orelse @panic("_start not found");
 
-    dbg("SYM: {}\n", .{sym});
-    if (sym.kind != .func) return dbg("not a function :(\n", .{});
+    if (sym.kind != .func) @panic("_start not a function :(");
 
-    const num = try std.fmt.parseInt(i32, std.mem.span(argv[3]), 10);
-    var res: [1]StackValue = undefined;
-    const n_res = try in.execute(sym.idx, &.{.{ .i32 = num }}, &res, true);
-    if (n_res != 1) dbg("TODO: n_res\n", .{});
-    dbg("{s}({}) == {}\n", .{ std.mem.span(argv[2]), num, res[0].i32 });
+    _ = in.execute(sym.idx, &.{}, &.{}, true) catch |err| {
+        if (err == error.WASMTrap) {
+            if (state.exit_status) |status| {
+                // TRAP was sent by wasi_proc_exit
+                return status;
+            }
+        }
+        return err;
+    };
+
+    return 0;
 }
 
 fn wasi_proc_exit(args_ret: []StackValue, in: *Instance, data: *anyopaque) !void {
-    _ = data;
+    const state: *WASIState = @ptrCast(@alignCast(data));
     _ = in;
-    const arg = args_ret[0].i32;
+    const arg = args_ret[0].u32();
     dbg("wasi exit: {}\n", .{arg});
+
     // std.posix.exit(0); // for benchmarking which expect 0 ret..
+    state.exit_status = arg;
     return error.WASMTrap;
 }
 
@@ -143,43 +192,15 @@ fn wasi_fd_write(args_ret: []StackValue, in: *Instance, data: *anyopaque) !void 
 }
 
 fn wasi_clock_time_get(args_ret: []StackValue, in: *Instance, data: *anyopaque) !void {
-    const klocka: *std.time.Timer = @ptrCast(@alignCast(data));
+    const state: *WASIState = @ptrCast(@alignCast(data));
     const id = args_ret[0].i32;
     const res_timestamp = try in.mem_get_bytes(args_ret[2].u32(), 8);
 
     if (id == 1) {
-        const time = klocka.read(); // PRESENT DAY, PRESENT TIME
+        const time = state.klocka.read(); // PRESENT DAY, PRESENT TIME
         std.mem.writeInt(u64, res_timestamp[0..8], time, .little);
     } else {
         dbg("hey guys check this out: {}\n", .{id});
         return error.WASMTrap;
     }
-}
-
-fn wasi_run(mod: *wasm_shelf.Module, allocator: std.mem.Allocator, stdin: ?[:0]const u8) !void {
-    if (stdin) |path| {
-        const fd = try std.posix.openZ(path, .{ .ACCMODE = .RDONLY }, 0);
-        try std.posix.dup2(fd, 0);
-    }
-
-    var imports: wasm_shelf.ImportTable = .init(allocator);
-    defer imports.deinit();
-
-    var klocka = try std.time.Timer.start();
-
-    try imports.add_func("proc_exit", .{ .cb = &wasi_proc_exit, .n_args = 1, .n_res = 0 });
-    try imports.add_func("fd_read", .{ .cb = &wasi_fd_read, .n_args = 4, .n_res = 1 });
-    try imports.add_func("fd_write", .{ .cb = &wasi_fd_write, .n_args = 4, .n_res = 1 });
-    try imports.add_func("clock_time_get", .{ .cb = &wasi_clock_time_get, .n_args = 3, .n_res = 1, .data = @ptrCast(&klocka) });
-
-    var in = try wasm_shelf.Instance.init(mod, &imports);
-
-    const sym = try mod.lookup_export("_start") orelse
-        return dbg("not a wasi module? :pensive:\n", .{});
-
-    if (sym.kind != .func) return dbg("not a function :(\n", .{});
-
-    _ = try in.execute(sym.idx, &.{}, &.{}, true);
-
-    defer in.deinit();
 }
